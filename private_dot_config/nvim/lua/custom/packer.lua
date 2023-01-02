97157b848df0eb6f74e1802c9e16471c8d91c56b
Dalton Dick <daltondick2010@gmail.com>
2023-01-02 14:39:09 -0600

    Change config for comment

--- private_dot_config/nvim/lua/custom/packer.lua
+++ private_dot_config/nvim/lua/custom/packer.lua
@@ -73,19 +73,11 @@ return require('packer').startup(function(use)
     "folke/which-key.nvim",
     config = function()
       require("which-key").setup {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
       }
     end
   }
 
-  use {
-    'numToStr/Comment.nvim',
-    config = function()
-      require('Comment').setup()
-    end
-  }
+  use('numToStr/Comment.nvim')
 
   use {
     'nvim-lualine/lualine.nvim',
